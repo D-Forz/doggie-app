bb822408da17fda2e0d7424dede6be63b3c1edda
duvan <duvanfoz@hotmail.com>
2022-11-21 13:39:19 -0500

    feat: CRUD products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,46 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[show edit update destroy]
+  def index
+    @products = Product.all
+  end
+
+  def show; end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit; end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product, notice: 'Product was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @product.update_attributes(params[:product])
+      redirect_to @product, notice: 'Product was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_url, status: :see_other
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description)
+  end
 end
