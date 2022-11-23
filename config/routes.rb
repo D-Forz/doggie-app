94c119a94556350a88fe06bbc526e1ff11ea96b1
duvan <duvanfoz@hotmail.com>
2022-11-23 08:00:54 -0500

    style: routes and home login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, path: '/', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
-  # Defines the root path route ("/")
   root 'pages#home'
   resources :products
 end
